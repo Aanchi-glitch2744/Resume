d9aeddaa57738263501a6d2118fb87e39e2ad252
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2022-06-26 17:52:53 +0530

    Toolbar created

--- resume-builder/src/pages/Create.js
+++ resume-builder/src/pages/Create.js
@@ -1,5 +1,34 @@
-import './create.css'
+import React from 'react';
+import { ArrowDown } from 'react-feather';
+import styles from './Create.module.css';
 
 export default function Create() {
-  return <h1>Template</h1>
+
+  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+ 
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.heading}>Resume Builder</p>
+      <div className={styles.toolbar}>
+        <div className={styles.colors}>
+          {
+            colors.map((item=>
+              <span
+              key={item}
+              style={{ backgroundColor: item }} 
+              className={styles.color} />
+              ))
+          }
+          {/* <span className={styles.color} />
+          <span className={styles.color} />
+          <span className={styles.color} />
+          <span className={styles.color} />
+           */}
+        </div>
+        <button>Download <ArrowDown/> </button>
+      </div>
+    </div>
+
+  )
 }
