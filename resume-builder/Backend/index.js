a9f4f017eeda514d9af2c66798c73cb490bd1dd0
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2022-08-02 22:25:17 +0530

    Responsive for mobile screen

--- resume-builder/Backend/index.js
+++ resume-builder/Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const PORT = 3000
+const PORT = 3333
 const {MONGOURI} = require('./config/keys')
 
 
